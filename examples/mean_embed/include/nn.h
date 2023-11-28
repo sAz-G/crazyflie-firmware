3f0806c0e45003143e643627c63c253eb7e2ab18
sAz-G <sharifazem49@gmail.com>
2023-11-28 22:58:07 +0100

    update nn structure

--- examples/mean_embed/include/nn.h
+++ examples/mean_embed/include/nn.h
@@ -1,25 +1,24 @@
 #ifndef __NN_H__
 #define __NN_H__
 
-#define K_NEIGHBOURS 6
-#define N_DRONES
-#define OBSERVATION_DIM
-#define OUTPUT_DIM
-#define SELF_ENCODER_HORIZONTAL_DIM
-#define SELF_ENCODER_VERTICAL_DIM
-#define NEIGHBOUR_ENCODER_HORIZONTAL_DIM
-#define NEIGHBOUR_ENCODER_VERTICAL_DIM
-#define FEED_FORWARD_VERTICAL_DIM
-#define FEED_FORWARD_HORIZONTAL_DIM
+#define K_NEIGHBOURS                            6
+#define N_DRONES                                8
+#define SELF_ENCODER_W                          {{16, 18}, {16,16}}
+#define SELF_ENCODER_B                          {16, 16}
+#define NEIGHB_ENCODER_W                        {{8, 6}, {8,8}}
+#define NEIGHB_ENCODER_B                        {8, 8}
+#define MLP_W                                   {{32, 24}, {32,32}}
+#define MLP_B                                   {4, 32}
 
-#define N_IN
-#define N_OUT
-#define PHI_S_V
-#define PHI_S_H
-#define PSI_ETA_V
-#define PSI_ETA_H
-#define FF_V
-#define FF_H
+
+#define N_IN        OBSERVATION_DIM
+#define N_OUT       OUTPUT_DIM
+#define PHI_S_H     SELF_ENCODER_HORIZONTAL_DIM
+#define PHI_S_V     SELF_ENCODER_VERTICAL_DIM
+#define PSI_ETA_H   NEIGHBOUR_ENCODER_HORIZONTAL_DIM
+#define PSI_ETA_V   NEIGHBOUR_ENCODER_VERTICAL_DIM
+#define FF_V        FEED_FORWARD_VERTICAL_DIM
+#define FF_H        FEED_FORWARD_HORIZONTAL_DIM
 
 
 
