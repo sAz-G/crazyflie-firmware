db57a3dd73ad60d88f1dca5f61cc0ea4df862ff2
saz <sharifazem49@gmail.com>
2023-11-28 18:35:11 +0100

    added files for neural networks

---
+++ examples/mean_embed/include/nn.h
@@ -0,0 +1,26 @@
+#ifndef __NN_H__
+#define __NN_H__
+
+#define K_NEIGHBOURS 6
+#define N_DRONES
+#define OBSERVATION_DIM
+#define OUTPUT_DIM
+#define SELF_ENCODER_HORIZONTAL_DIM
+#define SELF_ENCODER_VERTICAL_DIM
+#define NEIGHBOUR_ENCODER_HORIZONTAL_DIM
+#define NEIGHBOUR_ENCODER_VERTICAL_DIM
+#define FEED_FORWARD_VERTICAL_DIM
+#define FEED_FORWARD_HORIZONTAL_DIM
+
+#define N_IN
+#define N_OUT
+#define PHI_S_V
+#define PHI_S_H
+#define PSI_ETA_V
+#define PSI_ETA_H
+#define FF_V
+#define FF_H
+
+
+
+#endif
