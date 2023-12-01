b2e73dbe1476575522e4c48146261e7f2a005dbb
saz <sharifazem49@gmail.com>
2023-12-01 10:48:57 +0100

    started implementation of the last layer

---
+++ examples/mean_embed/include/mlp.h
@@ -0,0 +1,10 @@
+#ifndef __MLP_H__
+#define __MLP_H__
+
+#define INPUTDIM 24
+#define OUTPUT0 32
+#define OUTPUTDIM 4
+
+void calcMlpOutput();
+
+#endif
---
+++ examples/mean_embed/src/mlp.c
@@ -0,0 +1,5 @@
+#include "../include/mlp.h"
+
+static float* outputVec;
+static float* inputVec;
+
